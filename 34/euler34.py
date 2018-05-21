9649e577a492b6ec3c1d7dd6c3448114cef8cec0
Adam Karl <adamkkarl@gmail.com>
2018-05-21 14:14:38 -0400

    euler34 solution with header

---
+++ 34/euler34.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+__author__ = "Adam Karl"
+"""Find the sum of all numbers below N which divide the sum of the factorial of their digits"""
+#https://www.hackerrank.com/contests/projecteuler/challenges/euler034/copy-from/1307250873
+#Input: N
+#Constraints: 10 <= N <= 10**5
+
+digitFactorials = []
+
+def curiousNumbersUnderN(n):
+    sumCurious = 0
+    for i in range(10, n):
+        string = str(i)
+        digitFactorialSum = 0
+        for c in string:
+            digitFactorialSum += digitFactorials[int(c)]
+#        print(i, digitFactorialSum)
+        if digitFactorialSum % i == 0:
+            sumCurious += i
+    return sumCurious
+        
+
+
+def main():
+    global digitFactorials
+    digitFactorials = [1] #0! = 1
+    for i in range(9): #1-9
+        digitFactorials.append(digitFactorials[-1] * (i+1))
+        
+    n = int(input().strip())
+    result = curiousNumbersUnderN(n)
+    print(result)
+    
+
+if __name__ == "__main__":
+    main()
