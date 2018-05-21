3bc62ab5cc9dbba07c8aba90620a63e88d77ab92
Adam Karl <adamkkarl@gmail.com>
2018-05-21 13:30:25 -0400

    euler24 solution with header

---
+++ 24/euler24.py
@@ -0,0 +1,64 @@
+#!/bin/python3
+
+__author__ = "Adam Karl"
+"""What is the Nth lexiographic permutation of abcdefghijklm"""
+#https://www.hackerrank.com/contests/projecteuler/challenges/euler024/problem
+#first line T number of test cases, followed by T lines of N values
+#constraints: 1 <= T <= 1000; 1 <= N <= 13!
+
+factorials = []
+
+def generateFactorials():
+    for i in range(12): #0-12 for factorials 1! to 12!
+        factorials.append(factorial(i+1))
+        
+def factorial(n):
+    if n == 1:
+        return 1
+    return n * factorial(n-1)
+
+def printChars(letters, n):
+    if n == 1:#print out the rest normally
+        for c in letters:
+            print(c,end="")
+    elif len(letters) == 2:
+        if n == 2:
+            print(str(letters[1]) + str(letters[0]),end="")
+        else:
+            print("ERROR" + str(n))
+#        elif n == 0:
+#            print(str(lerrers[0]) + str(letters[1]))
+    
+        
+    else:
+    
+        length = len(letters) - 1 
+       # print(length)
+        combinationsForRestOfDigits = factorials[length-1]
+        index = 0
+
+        while n > combinationsForRestOfDigits:
+            index += 1
+            n -= combinationsForRestOfDigits
+
+#        print(index,end=" ")
+#        print(letters)
+
+        print(letters.pop(index),end="") #remove from list and print 
+   #     n -= index * combinationsForRestOfDigits
+        printChars(letters, n)
+
+
+def main():
+    generateFactorials()
+#    print(factorials)
+    t = int(input())
+    for a0 in range(t):
+        letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m']
+        n = int(input()) 
+        printChars(letters, n)
+        print()
+    
+
+if __name__ == "__main__":
+    main()
