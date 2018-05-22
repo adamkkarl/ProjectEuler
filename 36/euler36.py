973e37f9d2c100bbeca7c7e55cc72c2c3f588b8c
Adam Karl <adamkkarl@gmail.com>
2018-05-21 22:51:39 -0400

    updated to hackerrank solution

--- 36/euler36.py
+++ 36/euler36.py
@@ -1,6 +1,10 @@
 #!/bin/python3
 
 __author__ = "Adam Karl"
+"""Find the sum of all natural numbers less than N that are palindromes both in base 10 and base K"""
+#https://www.hackerrank.com/contests/projecteuler/challenges/euler036
+#First line has N K
+#Constraints: 10 <= N <= 10**6; 2 <= K <= 9
 
 def isPalindrome(s):
     """given a string s, recursively return True if it is a palindrome, False otherwise"""
@@ -8,18 +12,25 @@ def isPalindrome(s):
         return True
     return False
 
-def numPalindromes(n):
-    """Given int n, return the sum of the integers less than n that are palindromes both in base 10 and binary"""
+def returnBaseK(n, k):
+    """Given base 10 number n and base k, return n converted to base k as a string"""
+    s = ""
+    while n > 0:
+        s = s + str(n % k)
+        n  = int(round(n // k))
+    return s[::-1]
+    
+def numPalindromes(n, k):
+    """Given ints n and k, return the sum of the integers less than n that are palindromes both in base 10 and base k"""
     palindrome_sum = 0
     for i in range(n):
-        if isPalindrome(str(i)) and isPalindrome("{0:b}".format(i)):
+        if isPalindrome(str(i)) and isPalindrome(returnBaseK(i, k)):
             palindrome_sum += i
     return palindrome_sum
 
-
 def main():
-    n = int(input())
-    result = numPalindromes(n)
+    n, k = list(map(int, input().split()))
+    result = numPalindromes(n, k)
     print(result)
 
 if __name__ == "__main__":
