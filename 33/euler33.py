8b7e9efd8690b4ea057e6c42c99f42024503214f
Adam Karl <adamkkarl@gmail.com>
2018-05-23 14:03:57 -0400

    hackerrank solution for fractions with no repeated digits

--- 33/euler33.py
+++ 33/euler33.py
@@ -1,6 +1,12 @@
 #!/bin/python3
 
 __author__ = "Adam Karl"
+"""49/98 is curious since a naiive mathematician might simplify by just cancelling the '9's in the fraction leaving 4/8 (which is actually a valid simplification). Given N, the number of digits in the numerator and denominator, and K the number of integers to cancel, find every non-trivial solution and return the sum of the numerators and sum of the denominators"""
+#https://www.hackerrank.com/contests/projecteuler/challenges/euler033
+#Input: N K
+#Constraints: 2 <= N <= 4; 1 <= K <= N-1
+
+#TODO: allow for all permutations of removals: 4808/8414 => 8/14 for n=4, k=2
 
 def simplify(num, denom):
     """Given a numerator and denominator for a fraction, reduce it to lowest terms"""
@@ -26,35 +32,35 @@ def digitListToInt(arr):
         return int("".join(arr))
 
 def incorrectSimplify(num, denom):
-    """Given a numerator and denominator, reduce it incorrectly by cancelling out digits (e.g. the 4s cancel in 34/49 to give 3/9"""
+    """Given a numerator and denominator, reduce it incorrectly by cancelling out digits. Also return the number of pairs of digits canceled out (e.g. the 4s cancel in 34/49 to give 3/9, and returns 1 pair canceled)"""
     numList = [x for x in str(num)]
     denomList = [x for x in str(denom)]
+    digitsCanceled = 0
     for digit in numList:
         if digit in denomList and digit != '0':
             numList.remove(digit)
             denomList.remove(digit)
-    return [digitListToInt(numList), digitListToInt(denomList)]
+            digitsCanceled += 1
+    return [digitListToInt(numList), digitListToInt(denomList), digitsCanceled]
 
 def main():
-    numerators = []
-    denominators = []
-    for i in range(10, 100):
-        for j in range(i + 1, 100):
-            n1, n2 = simplify(i, j)
-            i1, i2 = incorrectSimplify(i, j)
-            i3, i4 = simplify(i1, i2)
-            if n1 == i3 and n2 == i4 and i1 != i and i1 != j:
-               numerators.append(i)
-               denominators.append(j)
-    print(numerators)
-    print(denominators)
-    totN = 1
-    for n in numerators:
-        totN *= n
-    totD = 1
-    for d in denominators:
-        totD *= d
-    print(simplify(totN, totD))
+    numeratorSum = 0
+    denominatorSum = 0
+
+    n, k = list(map(int, input().split()))
+
+    min_numerator = pow(10, n - 1)
+    max_numerator = pow(10, n)
+    for numerator in range(min_numerator, max_numerator):
+        for denominator in range(numerator + 1, max_numerator):
+            inc_num, inc_denom, test_k = incorrectSimplify(numerator, denominator)
+            if test_k == k:
+                inc_num, inc_denom = simplify(inc_num, inc_denom)
+                cor_num, cor_denom = simplify(numerator, denominator)
+                if inc_num == cor_num and inc_denom == cor_denom:
+                    numeratorSum += numerator
+                    denominatorSum += denominator
+    print(numeratorSum, denominatorSum)
 
 if __name__ == "__main__":
     main()
