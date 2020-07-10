349769798e721d9c38ea46de029526126769f6da
adamkkarl <adamkkarl@gmail.com>
2020-07-10 15:00:56 -0400

    refactored into projecteuler and hackerrank solutions

--- 01/hackerrank1.py
+++ 01/hackerrank1.py
@@ -3,6 +3,9 @@
 import sys
 
 #Problem 1 Project Euler Solution
+#Passes all test cases on HackerRank April 2018
+#Constraints: 1 <= T <= 10**5; 1 <= N <= 10**9
+#https://www.hackerrank.com/contests/projecteuler/challenges/euler001
 
 def sum_multiples_of_k(n, k):
     """Finds the sum of all multiples of k that are less than n"""
@@ -13,9 +16,12 @@ def sum_multiples_of_k(n, k):
     return (terms * pair_val) >> 1
     
 def main():    
-    print("Sum all natural numbers between that are multiples of 3 and 5 below: ", end="")
-    n = int(input().strip())
-    print("%d" % (sum_multiples_of_k(n, 3) + sum_multiples_of_k(n, 5) - sum_multiples_of_k(n, 15)))
+    t = int(input().strip())
+    for a0 in range(t):
+        n = int(input().strip())
+        
+        print("%d" % (sum_multiples_of_k(n, 3) + sum_multiples_of_k(n, 5) - sum_multiples_of_k(n, 15)))
 
 if __name__ == "__main__":
     main()
+
