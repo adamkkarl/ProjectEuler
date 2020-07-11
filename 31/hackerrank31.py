86d9eab544f991d744389392c9498ce9c0b514a5
adamkkarl <adamkkarl@gmail.com>
2020-07-11 17:35:21 -0400

    separated 31 solutions

--- 31/hackerrank31.py
+++ 31/hackerrank31.py
@@ -3,29 +3,32 @@
 __author__ = "Adam Karl"
 
 """Given an amount of money, find the number of unique ways to make that amount using English coins (up to 200p) mod 10**9 + 7"""
-#https://projecteuler.net/problem=31
+#https://www.hackerrank.com/contests/projecteuler/challenges/euler031
+#First line t number of test cases, followed by t lines of N values (currency amounts)
+#Constraints: 1 <= T <= 10 000; 1 <= N <= 100 000
 
 memo = [] #memo[n] has the number of ways to make n pence using coins
 coinValues = [1, 2, 5, 10, 20, 50, 100, 200]
-max = 100001
+MAXIMUM = 100001
+MOD = 1000000007
 
 def generateMemo():
     """fill memo by passing over it once for each progressively larger coin value to find combinations
     for N using up to each maximum coin"""
     global memo
-    memo = [1 for i in range(max+1)] #all ones for coinValue = 1
+    memo = [1 for i in range(MAXIMUM)] #all ones for coinValue = 1
     for coinIndex in range(1, 8):
         coinValue = coinValues[coinIndex]
-        for n in range(coinValue, max+1):
-            memo[n] = (memo[n] + memo[n - coinValue])
+        for n in range(coinValue, MAXIMUM):
+            memo[n] = (memo[n] + memo[n - coinValue]) % MOD
 
 def main():
-    global max
-    print("How many ways can you make __p out of 1p, 2p, 5p, 10p, 20p, 50p, £1 (100p), and £2 (200p)? ")
-    max = int(input())
     generateMemo()
-    result = memo[max]
-    print("%d ways" % result)
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        result = memo[n]
+        print(result)
 
 if __name__ == "__main__":
     main()
