b05b9c8704a3a74779a83fb62126efc22ae04390
adamkkarl <adamkkarl@gmail.com>
2020-07-11 19:07:04 -0400

    removed overhead from euler soln

--- 21/euler21.py
+++ 21/euler21.py
@@ -7,44 +7,44 @@ __author__ = "Adam Karl"
 from math import sqrt, ceil
 
 listOfAmicables = []
-sumOfDivisorsMemo = [-1] * 100000 #sumOfDivisors[n-1] has the sum of divisors for n 
+sumOfDivisorsMemo = []
 
-
-def generateAmicables():
+def generateAmicables(n):
     """Generate a list of amicable numbers <= 100 000"""   
-    for a in range(2, 100001):
-        b = sumOfDivisors(a)
-        if a != b and a == sumOfDivisors(b):
+    for a in range(2, n+1):
+        b = sumOfDivisors(a, n)
+        if a != b and a == sumOfDivisors(b, n):
             listOfAmicables.append(a)
 
-def sumOfDivisors(n):
-    """Return the sum of all divisors of N, including 1 and N"""
-    if n > 0 and n <= 100000 and sumOfDivisorsMemo[n-1] != -1:
-        return sumOfDivisorsMemo[n-1]
-    
+def sumOfDivisors(a, n):
+    """Return the sum of all divisors of a, including 1 and a"""
+    if a > 0 and a <= n and sumOfDivisorsMemo[a-1] != -1:
+        return sumOfDivisorsMemo[a-1]
     
     my_sum = 1
     testFac = 2
-    sq_root = ceil(sqrt(n))
-    while testFac <= sqrt(n):
-        if n % testFac == 0:
-            pair = int(n / testFac)
+    sq_root = ceil(sqrt(a))
+    while testFac <= sqrt(a):
+        if a % testFac == 0:
+            pair = int(a / testFac)
             if pair != testFac:
                 my_sum += testFac + pair
             else:
                 my_sum += testFac
         testFac += 1
-    if n <= 100000:
-        sumOfDivisorsMemo[n-1] = my_sum
+    if a <= n:
+        sumOfDivisorsMemo[a-1] = my_sum
     return my_sum
 
 def main():
     print("Sum all amicable numbers under: ", end="")
     n = int(input())
+    global sumOfDivisorsMemo
+    sumOfDivisorsMemo = [-1] * n #sumof
 
     sumOfDivisorsMemo[0] = 0 #n = 1 has sum of divisors of 0
     sumOfDivisorsMemo[1] = 1 #n = 2 has sum of divisors of 1
-    generateAmicables()
+    generateAmicables(n)
     numAmicables = len(listOfAmicables)
     
     sum_of_amicables = 0
