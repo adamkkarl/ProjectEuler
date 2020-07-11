a3fd6c4be2ad8e74887ef46cd4662ac5c85d30e4
adamkkarl <adamkkarl@gmail.com>
2020-07-11 17:46:26 -0400

    TODO unfinished

---
+++ 33/hackerrank33.py
@@ -0,0 +1,66 @@
+#!/bin/python3
+
+__author__ = "Adam Karl"
+"""49/98 is curious since a naiive mathematician might simplify by just cancelling the '9's in the fraction leaving 4/8 (which is actually a valid simplification). Given N, the number of digits in the numerator and denominator, and K the number of integers to cancel, find every non-trivial solution and return the sum of the numerators and sum of the denominators"""
+#https://www.hackerrank.com/contests/projecteuler/challenges/euler033
+#Input: N K
+#Constraints: 2 <= N <= 4; 1 <= K <= N-1
+
+#TODO: allow for all permutations of removals: 4808/8414 => 8/14 for n=4, k=2
+
+def simplify(num, denom):
+    """Given a numerator and denominator for a fraction, reduce it to lowest terms"""
+    while num % 2 == 0 and denom % 2 == 0:
+        num = num >> 1
+        denom = denom >> 1
+    
+    factor = 3
+    while factor <= num:
+        while num % factor == 0 and denom % factor == 0:
+            num = int(round(num / factor))
+            denom = int(round(denom / factor))
+        factor += 2
+    return [num, denom]
+
+def digitListToInt(arr):
+    """Given list of string digits, convert to integer. If empty return 0"""
+    if len(arr) == 0:
+        return 0
+    if len(arr) == 1:
+        return int(arr[0])
+    else:
+        return int("".join(arr))
+
+def incorrectSimplify(num, denom):
+    """Given a numerator and denominator, reduce it incorrectly by cancelling out digits. Also return the number of pairs of digits canceled out (e.g. the 4s cancel in 34/49 to give 3/9, and returns 1 pair canceled)"""
+    numList = [x for x in str(num)]
+    denomList = [x for x in str(denom)]
+    digitsCanceled = 0
+    for digit in numList:
+        if digit in denomList and digit != '0':
+            numList.remove(digit)
+            denomList.remove(digit)
+            digitsCanceled += 1
+    return [digitListToInt(numList), digitListToInt(denomList), digitsCanceled]
+
+def main():
+    numeratorSum = 0
+    denominatorSum = 0
+
+    n, k = list(map(int, input().split()))
+
+    min_numerator = pow(10, n - 1)
+    max_numerator = pow(10, n)
+    for numerator in range(min_numerator, max_numerator):
+        for denominator in range(numerator + 1, max_numerator):
+            inc_num, inc_denom, test_k = incorrectSimplify(numerator, denominator)
+            if test_k == k:
+                inc_num, inc_denom = simplify(inc_num, inc_denom)
+                cor_num, cor_denom = simplify(numerator, denominator)
+                if inc_num == cor_num and inc_denom == cor_denom:
+                    numeratorSum += numerator
+                    denominatorSum += denominator
+    print(numeratorSum, denominatorSum)
+
+if __name__ == "__main__":
+    main()
