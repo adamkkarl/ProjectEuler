a1444fdd710e70efeb6b7a47cebd6220d729ae27
adamkkarl <adamkkarl@gmail.com>
2020-07-11 16:54:48 -0400

    separated 25 solutions

--- 25/hackerrank25.py
+++ 25/hackerrank25.py
@@ -1,7 +1,9 @@
 #!/bin/python3
 
 """What is the first Fibonacci number to contain N digits"""
-#https://projecteuler.net/problem=25
+#https://www.hackerrank.com/contests/projecteuler/challenges/euler025/problem
+#First line T number of test cases, then T lines of N values
+#Constraints: 1 <= T <= 5000; 2 <= N <= 5000
 
 #fibNums = [1,1]
 fibIndex = [1] #fibIndex[n-1] has the index of the first fibonacci number with at least n digits
@@ -24,10 +26,11 @@ def generateFibonacciIndexes():
         index += 1
 
 def main():
-    print("Find index of the first Fibonacci number with n digits. n = ", end="")
-    n = int(input())
     generateFibonacciIndexes()
-    print("The first Fibonacci number with %d digits is at index %d" % (n, fibIndex[n-1]))
+    t = int(input())
+    for a0 in range(t):
+        n = int(input())
+        print(fibIndex[n-1])
 
 if __name__ == "__main__":
     main()
