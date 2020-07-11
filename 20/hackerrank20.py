84eff0b79e2aae1f1ac99a2265fefe24ce0e6d17
adamkkarl <adamkkarl@gmail.com>
2020-07-11 16:10:49 -0400

    separated 20 solutions

--- 20/hackerrank20.py
+++ 20/hackerrank20.py
@@ -2,7 +2,9 @@
 
 __author__ = "Adam Karl"
 """Find the sum of digits in the factorial of a number"""
-#https://projecteuler.net/problem=20
+#https://www.hackerrank.com/contests/projecteuler/challenges/euler020/problem
+#First line T number of test cases, followed by T lines of N values
+#Constraints: 1 <= T <= 100; 0 <= N <= 1000
 #April 2018
 
 from math import factorial
@@ -16,10 +18,11 @@ def sumOfDigits(n):
     return my_sum
     
 def main():
-    print("Find the sum of the digits of x! where x = ", end="")
-    n = int(input())
-    fac = factorial(n)
-    print("Sum of digits of %d! = %d" % (n, sumOfDigits(fac)))
+    t = int(input())
+    for a0 in range(t):
+        n = int(input())
+        fac = factorial(n)
+        print(sumOfDigits(fac))
         
 
 if __name__ == "__main__":
